15e219c47b3e17a22286f50500074d4344af23d6
Enes <arisoyeness@gmail.com>
2024-04-14 21:15:15 +0300

    Viewbinding enabled

--- app/src/main/java/com/ns/getirfinalcase/MainActivity.kt
+++ app/src/main/java/com/ns/getirfinalcase/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.ns.getirfinalcase
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.ns.getirfinalcase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
