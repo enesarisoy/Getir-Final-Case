faa8c296a3b27cb19231a1c3893499c70e6f9c59
Enes <arisoyeness@gmail.com>
2024-04-15 15:42:20 +0300

    Network Module and Constants

---
+++ app/src/main/java/com/ns/getirfinalcase/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.ns.getirfinalcase.di
+
+import com.ns.getirfinalcase.BuildConfig
+import com.ns.getirfinalcase.core.util.Constants.BASE_URL
+import com.ns.getirfinalcase.data.source.remote.service.ProductService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+            else HttpLoggingInterceptor.Level.NONE
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        OkHttpClient.Builder().apply {
+            callTimeout(40, TimeUnit.SECONDS)
+            connectTimeout(40, TimeUnit.SECONDS)
+            readTimeout(40, TimeUnit.SECONDS)
+            addInterceptor(httpLoggingInterceptor)
+            return build()
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .client(httpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(retrofit: Retrofit): ProductService {
+        return retrofit.create(ProductService::class.java)
+    }
+}
