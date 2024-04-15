5295599d9fe00b660cff95c12964e1995cb97ccc
Enes <arisoyeness@gmail.com>
2024-04-15 16:06:01 +0300

    Declared application class in Manifest to work with Hilt

--- app/src/main/java/com/ns/getirfinalcase/MainActivity.kt
+++ app/src/main/java/com/ns/getirfinalcase/MainActivity.kt
@@ -3,7 +3,9 @@ package com.ns.getirfinalcase
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ns.getirfinalcase.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
