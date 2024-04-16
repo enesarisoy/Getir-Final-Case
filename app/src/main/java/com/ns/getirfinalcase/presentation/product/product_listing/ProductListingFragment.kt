27b7a2a0407d1c6b8b3db12726046039bd5555c9
Enes <arisoyeness@gmail.com>
2024-04-16 15:03:01 +0300

    Product Listing ViewModel created. Getting products (local and remote) from fragment.

--- app/src/main/java/com/ns/getirfinalcase/presentation/product/product_listing/ProductListingFragment.kt
+++ app/src/main/java/com/ns/getirfinalcase/presentation/product/product_listing/ProductListingFragment.kt
@@ -1,17 +1,23 @@
 package com.ns.getirfinalcase.presentation.product.product_listing
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.GridLayoutManager
+import com.bumptech.glide.Glide
 import com.ns.getirfinalcase.R
 import com.ns.getirfinalcase.core.base.BaseFragment
 import com.ns.getirfinalcase.core.base.BaseResponse
 import com.ns.getirfinalcase.core.domain.ViewState
+import com.ns.getirfinalcase.core.util.gone
+import com.ns.getirfinalcase.core.util.visible
 import com.ns.getirfinalcase.databinding.FragmentProductListingBinding
 import com.ns.getirfinalcase.databinding.ItemProductListingBinding
 import com.ns.getirfinalcase.databinding.ItemProductListingViewBinding
@@ -27,15 +33,18 @@ class ProductListingFragment : BaseFragment<FragmentProductListingBinding>(
     FragmentProductListingBinding::inflate
 ) {
 
-    private val viewModel: ProductViewModel by viewModels()
+    private val viewModel: ProductListingViewModel by viewModels()
+    private val productsFromBasket = mutableListOf<Product>()
+    private var totalPrice: Double = 0.0
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initListener()
         initAdapters()
 
         getProducts()
+        getProductsFromCart()
     }
-
 
 
     private val toolbarAdapter = SingleRecyclerAdapter<ItemToolbarProductsBinding, String>(
@@ -51,6 +60,43 @@ class ProductListingFragment : BaseFragment<FragmentProductListingBinding>(
                 ivCart.setOnClickListener {
                     findNavController().navigate(R.id.action_productListingFragment_to_shoppingCartFragment)
                 }
+                viewModel.getProductsFromCart()
+                viewLifecycleOwner.lifecycleScope.launch {
+                    viewModel.getProductsFromCart.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                        .collect { viewState ->
+                            when (viewState) {
+                                is ViewState.Success -> {
+                                    linearCart.visible()
+                                    //TODO (Animate Cart)
+                                    totalPrice = 0.0
+                                    productsFromBasket.forEach {
+                                        totalPrice += it.price * it.quantity
+
+                                    }
+                                    tvPrice.text =
+                                        "₺${String.format("%.2f", totalPrice)}"
+
+                                    if (totalPrice == 0.0) {
+                                        //TODO (Animate Cart)
+
+                                        linearCart.gone()
+                                    }
+                                }
+
+                                is ViewState.Error -> {
+                                    Log.d("Database", viewState.error)
+
+                                }
+
+                                is ViewState.Loading -> {
+                                    Log.d("Database", "Loading")
+
+                                }
+                            }
+                        }
+                }
+
+
             }
         }
     )
@@ -71,52 +117,158 @@ class ProductListingFragment : BaseFragment<FragmentProductListingBinding>(
         }
     )
 
-    private val itemProductListingAdapter = SingleRecyclerAdapter<ItemProductListingViewBinding, Product>(
-        { inflater, _, _ ->
-            ItemProductListingViewBinding.inflate(
-                inflater,
-                binding.rvProductListingScreen,
-                false
-            )
-        },
-        { binding, item ->
-            binding.apply {
-                tvProductName.text = item.name
-                tvAttribute.text = item.attribute
-                tvPrice.text = item.priceText
+    private val itemProductListingAdapter =
+        SingleRecyclerAdapter<ItemProductListingViewBinding, Product>(
+            { inflater, _, _ ->
+                ItemProductListingViewBinding.inflate(
+                    inflater,
+                    binding.rvProductListingScreen,
+                    false
+                )
+            },
+            { binding, product ->
+                binding.apply {
+                    tvProductName.text = product.name
+                    tvAttribute.text = product.attribute
+                    tvPrice.text = product.priceText
+                    Glide.with(binding.root.context).load(product.imageURL).into(ivFood)
 
-                ivAdd.setOnClickListener {
+                    if (product.quantity > 0) {
+                        tvProductQuantity.text = product.quantity.toString()
+                        displayComponents(binding)
+                    } else {
+                        clearComponents(binding)
+                    }
+
+
+                    ivAdd.setOnClickListener {
+                        displayComponents(binding)
+                        product.quantity++
+                        viewModel.addToCart(product)
+                        ivDelete.isClickable = true
+
+                        tvProductQuantity.text = product.quantity.toString()
+
+                    }
+
+                    ivDelete.setOnClickListener {
+                        product.quantity--
+
+                        viewModel.deleteFromCart(product)
+                        tvProductQuantity.text = product.quantity.toString()
+
+                        if (product.quantity == 0) {
+                            ivDelete.isClickable = false
+                            clearComponents(binding)
+
+                        }
+
+                    }
+
 
                 }
+            }
+        )
 
+
+    private fun displayComponents(binding: ItemProductListingViewBinding) {
+        binding.apply {
+
+            linearLayout.visible()
+            ivAdd.background =
+                ResourcesCompat.getDrawable(
+                    root.resources,
+                    R.drawable.cart_add_icon_bg_when_clicked,
+                    null
+                )
+            tvProductQuantity.setBackgroundColor(
+                ContextCompat.getColor(
+                    root.context,
+                    R.color.bg_primary
+                )
+            )
+            ivDelete.setImageDrawable(
+                ContextCompat.getDrawable(
+                    root.context,
+                    R.drawable.ic_thrash
+                )
+            )
+        }
+    }
+
+    private fun clearComponents(binding: ItemProductListingViewBinding) {
+        binding.apply {
+            linearLayout.gone()
+            tvProductQuantity.setBackgroundColor(
+                ContextCompat.getColor(
+                    root.context,
+                    R.color.white
+                )
+            )
+            ivAdd.background =
+                ResourcesCompat.getDrawable(
+                    root.resources,
+                    R.drawable.cart_add_icon_background,
+                    null
+                )
+            tvProductQuantity.text = null
+            ivDelete.setImageDrawable(null)
+
+        }
+    }
+
+    private fun getProductsFromCart() {
+        binding.apply {
+            viewModel.getProductsFromCart()
+            viewLifecycleOwner.lifecycleScope.launch {
+                viewModel.getProductsFromCart.flowWithLifecycle(viewLifecycleOwner.lifecycle)
+                    .collect { viewState ->
+                        when (viewState) {
+                            is ViewState.Success -> {
+                                val response = viewState.result as BaseResponse.Success
+                                productsFromBasket.clear()
+                                productsFromBasket.addAll(response.data)
+                                Log.d("Database", response.data.toString())
+
+                            }
+
+                            is ViewState.Error -> {
+                                Log.d("Database", viewState.error)
+
+                            }
+
+                            is ViewState.Loading -> {
+                                Log.d("Database", "Loading")
+
+                            }
+                        }
+                    }
             }
         }
-    )
+    }
 
     private fun getProducts() {
         binding.apply {
-            viewModel.getAllProducts()
+            viewModel.getAllProductsFromApi()
             viewLifecycleOwner.lifecycleScope.launch {
                 viewModel.getAllProducts.flowWithLifecycle(viewLifecycleOwner.lifecycle)
                     .collect { viewState ->
                         when (viewState) {
                             is ViewState.Success -> {
-//                                val fromBasket = mutableListOf<Product>()
-//
-//                                viewModel.productsFromBasket.observe(viewLifecycleOwner) {
-//                                    it?.let { it1 -> fromBasket.addAll(it1) }
-//                                }
-                                val response = viewState.result as BaseResponse.Success
-//                                fromBasket.forEach { basketProduct ->
-//                                    if (basketProduct.quantity > 0) {
-//                                        response.data[0].products.firstOrNull { it.id == basketProduct.id }
-//                                            ?.let { product ->
-//                                                product.quantity = basketProduct.quantity
-//                                            }
-//                                    }
-//                                }
 
-                                itemProductListingAdapter.data = response.data[0].products.toMutableList()
+                                val response = viewState.result as BaseResponse.Success
+                                productsFromBasket.forEach { basketProduct ->
+                                    if (basketProduct.quantity > 0) {
+                                        response.data[0].products.firstOrNull { it.id == basketProduct.id }
+                                            ?.let { product ->
+                                                product.quantity = basketProduct.quantity
+                                            }
+                                    }
+                                }
+
+
+                                itemProductListingAdapter.data = response.data[0].products
+
                                 println(response.data.toString())
 
 
@@ -139,7 +291,7 @@ class ProductListingFragment : BaseFragment<FragmentProductListingBinding>(
 
 
     private fun initListener() {
-        binding.rvProductListingScreen.adapter =concatAdapter
+        binding.rvProductListingScreen.adapter = concatAdapter
     }
 
     private val concatAdapter = ConcatAdapter(
@@ -149,9 +301,8 @@ class ProductListingFragment : BaseFragment<FragmentProductListingBinding>(
 
     private fun initAdapters() {
         binding.apply {
-            toolbarAdapter.data = listOf("AddressAdapter")
-//            productListingAdapter.data = listOf(Product(id = "", price = 0.0))
-            productListingAdapter.data = listOf("")
+            toolbarAdapter.data = listOf("toolbarAdapter")
+            productListingAdapter.data = listOf("productListingAdapter")
         }
     }
 }
