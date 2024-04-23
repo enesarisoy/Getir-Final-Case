609ba04a78694e0c0dbf3657359a892d0e4fcc2b
Enes <arisoyeness@gmail.com>
2024-04-23 12:12:56 +0300

    App icon and animation

--- app/src/main/java/com/ns/getirfinalcase/presentation/SplashActivity.kt
+++ app/src/main/java/com/ns/getirfinalcase/presentation/SplashActivity.kt
@@ -3,8 +3,10 @@ package com.ns.getirfinalcase.presentation
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieAnimationView
 import com.ns.getirfinalcase.MainActivity
 import com.ns.getirfinalcase.R
+import com.ns.getirfinalcase.presentation.custom.CustomLoadingView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -15,8 +17,12 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        val progressBar = findViewById<CustomLoadingView>(R.id.progressBar)
+        val customLoadingView = progressBar.findViewById<LottieAnimationView>(R.id.lottieAnimationView)
+
         CoroutineScope(Dispatchers.Main).launch {
-            delay(3000L)
+            delay(4000L)
+            customLoadingView.cancelAnimation()
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
         }
